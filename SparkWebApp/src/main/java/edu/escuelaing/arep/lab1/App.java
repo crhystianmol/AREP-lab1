e593d51dff4d5815607a587b4150898a8019edab
crhystian <crhystiancamilo@hotmail.com>
2021-08-15 01:01:31 -0500

    segundo commit

--- SparkWebApp/src/main/java/edu/escuelaing/arep/lab1/App.java
+++ SparkWebApp/src/main/java/edu/escuelaing/arep/lab1/App.java
@@ -1,4 +1,7 @@
 package edu.escuelaing.arep.lab1;
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -6,8 +9,55 @@ package edu.escuelaing.arep.lab1;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    /**
+     * This main method uses SparkWeb static methods and lambda functions to
+     * create a simple Hello World web app. It maps the lambda function to the
+     * /hello relative URL.
+     */
+    public static void main(String[] args) {
+        staticFiles.location("/public");
+        port(getPort());
+        get("/inputdata", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
+    }
+
+    private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>HTML Forms</h2>"
+                + "<form action=\"/results\">"
+                + "  First name:<br>"
+                + "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">"
+                + "  <br>"
+                + "  Last name:<br>"
+                + "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {
+        return req.queryParams("firstname") + " " +
+                req.queryParams("lastname");
+    }
+
+    /**
+     * This method reads the default port as specified by the PORT variable in
+     * the environment.
+     *
+     * Heroku provides the port automatically so you need this to run the
+     * project on Heroku.
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
