866bf62890e3a3e7021659315061cc6a5ac1565a
Crhystian Molano <crhystian.molano@mail.escuelaing.edu.co>
2021-08-24 22:36:17 -0500

    nueva entrega

--- SparkWebApp/src/main/java/edu/escuelaing/arep/HttpService.java
+++ SparkWebApp/src/main/java/edu/escuelaing/arep/HttpService.java
@@ -12,13 +12,19 @@ public class HttpService extends HttpConnection{
         return "https://www.alphavantage.co/query?function="+time+"&symbol="+stock+"&interval=5min&apikey=R1CH8N3UDQYTWUEQ";
     }
 
-
+    /**
+     * Acciones las cual queremos consultar
+     * @param input Accion que consultaremos en este caso (MSFT,IBM)
+     */
     @Override
     public void setStock(String input) {
         this.stock=input;    }
 
 
-
+    /**
+     * Es para ingresar el tiempo que vamos a consultar
+     * @param input el tiempo que vamos a consultar en este caso(TIME_SERIES_INTRADAY,TIME_SERIES_DAILY,TIME_SERIES_WEEKLY,TIME_SERIES_MONTHLY)
+     */
     @Override
     public void setTime(String input) {
         this.time = input;
